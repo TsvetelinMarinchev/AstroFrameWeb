a51c14132918e8003073909ffe06ab97a4f24e42
TsvetelinMarinchev <tsvetelinmar@abv.bg>
2025-07-15 20:54:50 +0300

    Adding ImageUrl for StarType and StarTypeSeeder

--- AstroFrameWeb.Data/Seeds/StarTypeSeeder.cs
+++ AstroFrameWeb.Data/Seeds/StarTypeSeeder.cs
@@ -15,11 +15,11 @@ namespace AstroFrameWeb.Data.Seeds
             {
                 var types = new[]
                 {
-                    new StarType { Name = "G-type", Description = "Like the Sun" },
-                    new StarType { Name = "M-type", Description = "Cool red dwarfs" },
-                    new StarType { Name = "O-type", Description = "Hot and massive" },
-                    new StarType { Name = "B-type", Description = "Blue and bright" },
-                    new StarType { Name = "K-type", Description = "Orange main sequence" }
+                    new StarType { Name = "G-type", ImageUrl = "/images/StartypeG", Description = "Like the Sun" },
+                    new StarType { Name = "M-type", ImageUrl = "/images/StartypeM", Description = "Cool red dwarfs" },
+                    new StarType { Name = "O-type", ImageUrl = "/images/StartypeO", Description = "Hot and massive" },
+                    new StarType { Name = "B-type", ImageUrl = "/images/StartypeB", Description = "Blue and bright" },
+                    new StarType { Name = "K-type", ImageUrl = "/images/StartypeK", Description = "Orange main sequence" }
                 };
 
                 dbContext.StarTypes.AddRange(types);
