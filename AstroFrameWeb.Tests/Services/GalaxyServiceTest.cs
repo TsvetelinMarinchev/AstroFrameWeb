48a7e0894d0dab36944883fc8706bf041c2b3222
TsvetelinMarinchev <tsvetelinmar@abv.bg>
2025-08-02 20:59:57 +0300

    FirstTest add Galaxy

--- AstroFrameWeb.Tests/Services/GalaxyServiceTest.cs
+++ AstroFrameWeb.Tests/Services/GalaxyServiceTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AstroFrameWeb.Data;
+using AstroFrameWeb.Data.Enums;
+using AstroFrameWeb.Data.Models.ViewModels;
+using AstroFrameWeb.Services.Implementations;
 using Microsoft.EntityFrameworkCore;
 
 namespace AstroFrameWeb.Tests.Services
@@ -17,7 +20,29 @@ namespace AstroFrameWeb.Tests.Services
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                .UseInMemoryDatabase(databaseName: "TestGalaxyDb")
                .Options;
+            var context = new ApplicationDbContext(options);
+            var galaxyService = new GalaxyService(context);
 
+            var model = new GalaxyCreateViewModel
+            {
+                Name = "Andromeda",
+                Description = "Spiral galaxy",
+                GalaxyType = GalaxyType.Spiral,
+                NumberOfStars = 1000000,
+                DistanceFromEarth = 2500000,
+                ImageUrl = "https://example.com"
+            };
+
+            await galaxyService.CreateGalaxyAsync(model, "test-user");
+
+            //prverka
+            var galaxy = context.Galaxies.FirstOrDefault(g => g.Name == "Andromeda");
+
+            Assert.NotNull(galaxy);
+            Assert.Equal("test-user", galaxy.CreatorId);
+            Assert.Equal(1000000, galaxy.NumberOfStars);
         }
+
+
     }
 }
